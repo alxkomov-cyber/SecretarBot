3ad287dde7d7e12e72e28e600b0feafd8b53fb3f
alxkomov-cyber <alx.komov@gmail.com>
2026-02-07 14:03:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Бот V6.2 (Requests) готов.")
 
 if __name__ == '__main__':
-    # keep_alive() 
+    keep_alive() 
     
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     application.add_handler(CommandHandler('start', start))
@@ -264,3 +264,4 @@ if __name__ == '__main__':
     
     print("Бот V6.2 запущен локально!")
     application.run_polling()
+
