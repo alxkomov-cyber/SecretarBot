8c35b40e733e17c3558322e3e0dd0022f8f82630
alxkomov-cyber <alx.komov@gmail.com>
2026-03-09 19:30:17 +0300

    Add ALLOWED_USER_ID to environment variables

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_DB_ID = os.getenv("NOTION_DATABASE_ID")
-
-# ID базы знаний (пока заглушка, в будущем сюда впишем ID новой базы)
 KNOWLEDGE_DB_ID = os.getenv("KNOWLEDGE_DB_ID", None)
+ALLOWED_USER_ID = os.getenv("ALLOWED_USER_ID")
